81428efb19fef1ae544793258b60493717a38c8b
Alex.Vilca <alexander.vilca.t@tecsup.edu.pe>
2026-06-01 10:32:49 -0500

    Commit 4: Implementacion de manipulacion de camara

--- app/src/main/java/com/example/usodemapas/MapScreen.kt
+++ app/src/main/java/com/example/usodemapas/MapScreen.kt
@@ -3,9 +3,11 @@ package com.example.usodemapas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -21,6 +23,13 @@ fun MapScreen() {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
 
+    LaunchedEffect(Unit) {
+        cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984, -71.6836503), 12f), // Ubicación Yura [cite: 31]
+            durationMs = 3000
+        )
+    }
+
 
     Box(modifier = Modifier.fillMaxSize()) {
         // Añadir GoogleMap al layout
@@ -28,12 +37,27 @@ fun MapScreen() {
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Denver, Colorado
+            // 1. Marcador básico en Arequipa de color azul personalizado
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
-                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE), // Icono azul
-                title = "Arequipa, Perú"
+                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE), // Icono azul [cite: 24]
+                title = "Arequipa, Perú",
+            snippet = "Punto de inicio guiado"
             )
+
+            // 2. Múltiples marcadores (Iteración de lista de ubicaciones)
+            val locations = listOf(
+                LatLng(-16.433415, -71.5442652),  // JLByR
+                LatLng(-16.4205151, -71.4945209), // Paucarpata
+                LatLng(-16.3524187, -71.5675994)  // Zamacola
+            )
+            locations.forEach { location ->
+                Marker(
+                    state = rememberMarkerState(position = location),
+                    title = "Ubicación de Interés",
+                snippet = "Punto de interés de la ciudad"
+                )
+            }
 
         }
     }
