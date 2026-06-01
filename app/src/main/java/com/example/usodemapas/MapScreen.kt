778966380f79a2d8ef6060096c2150d05a86fc0e
Alex.Vilca <alexander.vilca.t@tecsup.edu.pe>
2026-06-01 10:45:02 -0500

    Commit 5: Trazado de Poligonos

--- app/src/main/java/com/example/usodemapas/MapScreen.kt
+++ app/src/main/java/com/example/usodemapas/MapScreen.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.google.maps.android.compose.Polygon
+import com.google.maps.android.compose.Polyline
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -55,9 +58,51 @@ fun MapScreen() {
                 Marker(
                     state = rememberMarkerState(position = location),
                     title = "Ubicación de Interés",
-                snippet = "Punto de interés de la ciudad"
+                    snippet = "Punto de interés de la ciudad"
                 )
             }
+
+            val mallAventuraPolygon = listOf(
+                LatLng(-16.432292, -71.509145),
+                LatLng(-16.432757, -71.509626),
+                LatLng(-16.433013, -71.509310),
+                LatLng(-16.432566, -71.508853)
+            )
+
+
+            val parqueLambramaniPolygon = listOf(
+                LatLng(-16.422704, -71.530830),
+                LatLng(-16.422920, -71.531340),
+                LatLng(-16.423264, -71.531110),
+                LatLng(-16.423050, -71.530600)
+            )
+
+            val plazaDeArmasPolygon = listOf(
+                LatLng(-16.398866, -71.536961),
+                LatLng(-16.398744, -71.536529),
+                LatLng(-16.399178, -71.536289),
+                LatLng(-16.399299, -71.536721)
+            )
+
+
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+            fillColor = Color.Blue.copy(alpha = 0.4f), // Modificado para visibilidad transparente
+            strokeWidth = 5f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+            fillColor = Color.Blue.copy(alpha = 0.4f),
+            strokeWidth = 5f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+            fillColor = Color.Blue.copy(alpha = 0.4f),
+            strokeWidth = 5f
+            )
 
         }
     }
